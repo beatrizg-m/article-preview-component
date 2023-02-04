620bec3864ca55dd0140139e0833737273f4dec1
be <beatrizg.m@yahoo.com>
2023-02-04 13:44:45 -0300

    Share button adjustment

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const userInformation = document.querySelector('.user');
 const share = document.querySelector('.share')
-const buttonShare = document.querySelectorAll('.button-share')
-console.log(share);
+const buttonShare = document.querySelector('.button-share')
+
 
 userInformation.addEventListener('click', (e) => {
     e.preventDefault();
     userInformation.classList.add('disable');
+    buttonShare.classList.add('active');
     share.classList.remove('disable');
 }) 
 
 share.addEventListener('click', (e) => {
     userInformation.classList.remove('disable');
+    buttonShare.classList.remove('active');
     share.classList.add('disable');
 })
 
